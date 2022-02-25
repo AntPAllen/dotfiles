ba2636573cda55a80f0d49e43327115f621d4256
Anthony Allen <anthony.allen@snowsoftware.com>
2022-02-25 11:22:33 +0100

    add some more keymaps

--- home-manager/configs/neovim/init.lua
+++ home-manager/configs/neovim/init.lua
@@ -174,10 +174,10 @@ vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin
 vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').git_status()<cr>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gp', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gic', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gib', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gis', [[<cmd>lua require('telescope.builtin').git_status()<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gip', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>wo', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]], { noremap = true, silent = true })
 
 -- Managing quickfix list
@@ -190,6 +190,17 @@ vim.cmd [[autocmd FileType qf nnoremap <buffer> q :lclose <bar> cclose <CR> ]]
 -- Toggle
 vim.api.nvim_set_keymap('n', '<leader>tf', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tt', ':ToggleTerm<CR>', { noremap = true, silent = true })
+
+-- Fugitive shortcuts
+vim.api.nvim_set_keymap('n', '<leader>gia', ':Git add %:p<CR><CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gig', ':GBrowse<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gid', ':Gdiff<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gie', ':Gedit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gir', ':Gread<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>giw', ':Gwrite<CR><CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gil', ':silent! Glog<CR>:bot copen<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gim', ':Gmove<Space>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gio', ':Git checkout<Space>', { noremap = true, silent = true })
 
 -- Treesitter configuration
 -- Parsers must be installed manually via :TSInstall
